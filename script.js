afe0cb8978fe036e6031c891ba6641747f9d8eba
Danik <daniknewgarden@gmail.com>
2020-01-07 12:18:59 +0200

    Finished app

--- script.js
+++ script.js
@@ -1,41 +1,113 @@
-// let money = prompt("Ваш бюджет на месяц?", ""),
-//     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money, time;
 
-// let appData = {
-//     budget: money,
-//     timeData: time,
-//     expenses: {},
-//     optionalExpenses: {},
-//     income: [],
-//     savings: false
+function data() {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+data();
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+function addExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+
+        if ((typeof (a)) === "string" && (typeof (a)) != null && (typeof (b)) !=
+            null && a != '' && b != '') {
+            console.log('ok,all is good');
+            appData.expenses.a = b;
+        } else {
+            i = i - 1;
+        }
+    };
+}
+addExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(0);
+
+alert('Бюджет на день: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 1000) {
+    alert('У тебя низкий уровень дохода');
+} else if (appData.moneyPerDay < 2000) {
+    alert('У тебя средний уровень дохода');
+} else if (appData.moneyPerDay > 2000) {
+    alert('У тебя высокий уровень дохода', '')
+} else {
+    alert('Произошла ошибка', '');
+}
+
+appData.savings = confirm('У вас есть накопления?');
+console.log(appData.savings);
+
+if (appData.savings == true) {
+
+    let addDeposit = confirm('Хотите прибавить деньги?');
+
+    if (addDeposit == true) {
+        function deposit(savings, percent) {
+            
+            savings = +prompt('Какова сумма накоплений сейчас?', '');
+            percent = +prompt('Выберите процент от 1 до 25', '');
+
+            while (isNaN(percent) || percent == null || percent == '' || percent > 25) {
+                percent = +prompt('Я же сказал выберите процент от 1 до 25', '');
+            }
+
+            appData.incomeFromDeposit = savings / 100 / 12 * percent;
+            alert('Ваша прибыль в месяц: ' + appData.incomeFromDeposit);
+        }
+        deposit();
+    } else {
+        alert('Ну ладно');
+    }
+}
+
+
+
+
+// let hello = 'hello';
+
+// function showFirstMessage (text) {
+//     alert(text);
+//     hello = 35;
+// }
+// showFirstMessage('hello world');
+// alert(hello);
+
+// let calc = function(a, b) {
+//     a = +prompt('first number', '');
+//     b = +prompt('second number', '');
+//     return (a + b);
+// }
+
+// let calc = (a, b) =>  {
+//     a = +prompt('first number', '');
+//     b = +prompt('second number', ''); 
+//     return(a+b);
 // };
 
-// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//     a2 = prompt("Во сколько обойдется?", ""),
-//     a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//     a4 = prompt("Во сколько обойдется?", "");
+// alert('Ваш результат ' + calc());
 
-// appData.expenses.a1 = a2;
-// appData.expenses.a3 = a4;
+// let string = 'LOREM IPSUM';
 
-// alert(appData.budget / 30);
+// console.log(string.length);
+// console.log(string.toUpperCase());
+// console.log(string.toLowerCase());
 
-var num = prompt('Вгадай моє число від 0 до 100', '10');
-
-    if (num > 25) {
-        num = prompt('Забагато спробуй ще раз', '')
-    } else if (num < 25) {
-        num = prompt('Замало спробуй ще раз', '')
-    } else {
-        alert('ТакБ ти вагадав. Це 25')
-    }
-
-    // do{
-    //     if (num > 25) {
-    //         num = prompt('Забагато спробуй ще раз', '')
-    //     } else if (num < 25) {
-    //         num = prompt('Замало спробуй ще раз', '')
-    //     }
-    // }   while (25 > num, num > 25);
-
-    // alert('Yes, you are right');
+// let num = '15.6';
+// console.log(Math.round(num));
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
