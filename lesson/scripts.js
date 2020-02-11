2bd39e66736793803e455a6693b40399383be549
Danik <daniknewgarden@gmail.com>
2020-02-11 21:21:45 +0200

    lesson events and them listeners

---
+++ lesson/scripts.js
@@ -0,0 +1,27 @@
+let btn = document.querySelectorAll('button');
+
+// btn[0].onclick = function() {
+//     alert('You was clicked on button');
+// };
+// btn[0].onclick = function () {
+//     alert('You was clicked on button again');
+// };
+
+// btn[0].addEventListener('click', function (params) {
+//     alert('You was clicked on button');
+// });
+// btn[0].addEventListener('click', function (params) {
+//     alert('You was clicked on button again');
+// });
+// btn[1].addEventListener('click', function (event) {
+//     console.log(event);
+//     let target = event.target;
+//     target.style.display = 'none';
+//     console.log('Произошло событые ' + event.type + ' на элементе ' + target);
+// });
+
+btn.forEach(function (item) {
+   item.addEventListener('mouseleave', function (params) {
+       console.log('moudeleave');
+   });
+});
